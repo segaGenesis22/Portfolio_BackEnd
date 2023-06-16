179127cdabb7baa2af6c167f86e4f1f7815cb8bc
iGen22 <guerratavarezg@gmail.com>
2023-06-16 18:50:20 -0400

    configuring psql connection

--- service/dbConnection.js
+++ service/dbConnection.js
@@ -1,11 +1,22 @@
 const { Client } = require("pg");
 
-const client = new Client({
-    connectionString: process.env.HEROKU_POSTGRESQL_RED_URL,
-    ssl: {
-      rejectUnauthorized: false
-    }
-  });
+const config = {
+    user: "zmtuforniykarf",
+    database: "d8cfrbc7sqgi2r",
+    password: "098539232b66e6655827d87c2bd8b7c7f831b72295e2ae50f39e6470716959ad",
+    port: 5432,
+    max: 10, // max number of clients in the pool
+    idleTimeoutMillis: 30000,
+  };
+
+const client = new Client.Pool(config);
+
+//const client = new Client({
+    //connectionString: process.env.HEROKU_POSTGRESQL_RED_URL,
+    //ssl: {
+      //rejectUnauthorized: false
+    //}
+  //});
 
 client.on("connect", () => {
     console.log("Connected to Postgres database.")

4723a28a07ebd8c091e1c45992f4f5097c0568b1
iGen22 <guerratavarezg@gmail.com>
2023-06-16 00:29:50 -0400

    adding ssl certificates

--- services/dbConnection.js
+++ services/dbConnection.js
@@ -1,24 +1,19 @@
-const pg = require("pg");
+const { pg } = require("pg");
 
-const config = {
-    adapter: 'sails-mysql',
-    user: "zmtuforniykarf",
-    host: "ec2-18-211-172-50.compute-1.amazonaws.com",
-    database: "d8cfrbc7sqgi2r",
-    password: "098539232b66e6655827d87c2bd8b7c7f831b72295e2ae50f39e6470716959ad",
-    port: 5432,
-    max: 10,
-};
+const client = new pg({
+    connectionString: process.env.HEROKU_POSTGRESQL_RED_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });
 
-const pool = new pg.Pool(config);
-
-pool.on("connect", () => {
+client.on("connect", () => {
     console.log("Connected to Postgres database.")
 });
 
 const createTables = () => {
     const personTable = 'CREATE TABLE IF NOT EXISTS person(id INT auto_increment PRIMARY KEY, name VARCHAR(50) NOT NULL, email VARCHAR(100) NOT NULL, number INT NOT NULL, employer VARCHAR(100) NOT NULL)';
-    pool
+    client
         .query(personTable)
         .then((res) => {
             console.log(res);
@@ -30,17 +25,10 @@ const createTables = () => {
         });
 };
 
-pool.on("remove", () => {
+client.on("remove", () => {
     console.log("client removed");
     process.exit(0);
 });
-
-const client = new pg({
-    connectionString: process.env.HEROKU_POSTGRESQL_RED_URL,
-    ssl: {
-      rejectUnauthorized: false
-    }
-  });
 
 module.exports = {
     createTables,

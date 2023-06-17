8bd8721a039b8d6ab95bea1ee90c00d4a4c0bdc8
iGen22 <guerratavarezg@gmail.com>
2023-06-16 21:15:43 -0400

    working connection to Postgres database, deploying to heroku

--- service/dbConnection.js
+++ service/dbConnection.js
@@ -1,23 +1,18 @@
 const Client = require("pg");
 
 const config = {
+    adapter: "sails-postgresql",
     host: "ec2-18-211-172-50.compute-1.amazonaws.com",
     user: "zmtuforniykarf",
     database: "d8cfrbc7sqgi2r",
     password: "098539232b66e6655827d87c2bd8b7c7f831b72295e2ae50f39e6470716959ad",
     port: 5432,
+    ssl: {rejectUnauthorized: false},
     max: 10, // max number of clients in the pool
     idleTimeoutMillis: 30000,
 }; 
 
 const client = new Client.Pool(config);
-
-// const client = new Client({
-//     connectionString: process.env.HEROKU_POSTGRESQL_RED_URL,
-//     ssl: {
-//       rejectUnauthorized: false
-//     }
-// });
 
 client.on("connect", () => {
     console.log("Connected to Postgres database.")

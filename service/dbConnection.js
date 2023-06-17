f7d7e8af6b7862788f898679165aed47297ec193
iGen22 <guerratavarezg@gmail.com>
2023-06-16 20:58:28 -0400

    fixing ssl configuration

--- service/dbConnection.js
+++ service/dbConnection.js
@@ -1,22 +1,23 @@
-const { Client } = require("pg");
+const Client = require("pg");
 
 const config = {
+    host: "ec2-18-211-172-50.compute-1.amazonaws.com",
     user: "zmtuforniykarf",
     database: "d8cfrbc7sqgi2r",
     password: "098539232b66e6655827d87c2bd8b7c7f831b72295e2ae50f39e6470716959ad",
     port: 5432,
     max: 10, // max number of clients in the pool
     idleTimeoutMillis: 30000,
-  };
+}; 
 
 const client = new Client.Pool(config);
 
-//const client = new Client({
-    //connectionString: process.env.HEROKU_POSTGRESQL_RED_URL,
-    //ssl: {
-      //rejectUnauthorized: false
-    //}
-  //});
+// const client = new Client({
+//     connectionString: process.env.HEROKU_POSTGRESQL_RED_URL,
+//     ssl: {
+//       rejectUnauthorized: false
+//     }
+// });
 
 client.on("connect", () => {
     console.log("Connected to Postgres database.")
@@ -28,11 +29,11 @@ const createTables = () => {
         .query(personTable)
         .then((res) => {
             console.log(res);
-            pool.end();
+            client.end();
         })
         .catch((err) => {
             console.log(err);
-            pool.end();
+            client.end();
         });
 };
 

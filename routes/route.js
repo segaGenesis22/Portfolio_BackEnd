71e392d7e0a3cd85273047291253359dd2a06455
iGen22 <guerratavarezg@gmail.com>
2023-06-23 22:25:19 -0400

    added async routes

--- routes/route.js
+++ routes/route.js
@@ -1,15 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-require("dotenv").config();
-const db = require("../service/dbConnection.js");
-
-router.get("/", async (req, res) => {
+router.get('/', async (req, res, next) => {
     try {
         res.json({message: "Server Response."})
     } catch (error) {
         res.status(500).json({message: err.message})
     }
+    next()
 });
 
 module.exports = router;

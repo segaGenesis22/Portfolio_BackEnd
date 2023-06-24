faae30f82c74ed5a6c0ec8ed4d7c6c01fc3e72de
iGen22 <guerratavarezg@gmail.com>
2023-06-23 21:37:49 -0400

    adding cors

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const app = express();
 const routes = require("./routes/route.js");
 
 const bodyParser = require("body-parser");
+const cors = require('cors');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use("/", routes);
 

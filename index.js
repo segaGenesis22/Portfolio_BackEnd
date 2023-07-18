2f8c3b49dde19d7cd81d97af4c5f673f7bb2d704
iGen22 <guerratavarezg@gmail.com>
2023-07-17 21:39:11 -0400

    changed express configuration

--- index.js
+++ index.js
@@ -1,47 +1,35 @@
 require("dotenv").config();
-
 const http = require("http");
+
 const app = require("./app.js");
 
-const normalizePort = val => {
-    const port = parseInt(val, 10);
-    if (isNaN(port)) {
-        return val;
-    }
-    if (port >= 0) {
-        return port;
-    }
-    return false;
-};
+// const errorHandler = error => {
+//     if (error.syscall !== 'listen') {
+//       throw error;
+//     }
+//     const address = server.address();
+//     const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+//     switch (error.code) {
+//       case 'EACCES':
+//         console.error(bind + ' requires elevated privileges.');
+//         process.exit(1);
+//         break;
+//       case 'EADDRINUSE':
+//         console.error(bind + ' is already in use.');
+//         process.exit(1);
+//         break;
+//       default:
+//         throw error;
+//     }
+//   };
 
-const port = normalizePort(process.env.PORT || '3001');
-app.set('port', port);
+// const server = http.createServer(app);
 
-const errorHandler = error => {
-    if (error.syscall !== 'listen') {
-      throw error;
-    }
-    const address = server.address();
-    const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
-    switch (error.code) {
-      case 'EACCES':
-        console.error(bind + ' requires elevated privileges.');
-        process.exit(1);
-        break;
-      case 'EADDRINUSE':
-        console.error(bind + ' is already in use.');
-        process.exit(1);
-        break;
-      default:
-        throw error;
-    }
-  };
+// server.on('error', errorHandler);
+// server.on('listening', () => {
+//     const address = server.address();
+//     const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+//     console.log('Listening on ' + bind);
+// });
 
-const server = http.createServer(app);
-
-server.on('error', errorHandler);
-server.on('listening', () => {
-    const address = server.address();
-    const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
-    console.log('Listening on ' + bind);
-});
+app.listen(process.env.PORT || 3001, () => console.log('Server Started')) 

c342e14828fa53c09d5684183c277034c37bffd5
iGen22 <guerratavarezg@gmail.com>
2023-07-25 18:48:42 -0400

    testing new route

--- routes/route.js
+++ routes/route.js
@@ -4,6 +4,7 @@ const router = express.Router();
 router.get('/', async (req, res, next) => {
     try {
         res.json({message: "Server Response."})
+        console.log("Here is the server response")
     } catch (error) {
         res.status(500).json({message: err.message})
     }

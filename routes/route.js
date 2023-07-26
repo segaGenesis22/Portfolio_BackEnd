c43515d7ad9b29818f726e08410e5e69ee8233af
iGen22 <guerratavarezg@gmail.com>
2023-07-25 23:11:56 -0400

    changed path response

--- routes/route.js
+++ routes/route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.get('/', async (req, res, next) => {
     try {
-        res.send("Server Response.")
+        res.status(200).send("Server Response.")
     } catch (error) {
         res.status(500).json({message: err.message})
     }

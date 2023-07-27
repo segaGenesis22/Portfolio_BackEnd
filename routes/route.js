40fa6c8b0b530494b1502effac67808286d8a7b7
iGen22 <guerratavarezg@gmail.com>
2023-07-26 22:09:55 -0400

    removed unnecessary callback

--- routes/route.js
+++ routes/route.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', async (req, res, next) => {
+router.get('/', async (req, res) => {
     try {
         res.status(200).send("Server Response.")
     } catch (error) {
         res.status(500).json({message: err.message})
     }
-    next()
 });
 
 module.exports = router;
